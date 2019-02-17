d3da1ced11f93895cb98f1b776f5b1b2de2ce4d6
Bérénice Batut <bebatut@gmail.com>
2019-02-17 17:26:40 +0100

    Use Bulma and update website

---
+++ README.md
@@ -0,0 +1,18 @@
+Slides for talks - Bérénice Batut
+=================================
+
+This repository served the slides for my talks.
+
+# Usage
+
+## Install the requirements
+
+1. Open a Terminal
+2. (If not done yet) Clone the training material GitHub repository: `git clone https://github.com/bebatut/talks.git`
+3. Navigate to the `talks/` folder with `cd`
+4. Install Jekyll and related modules into the conda environment: `make install`
+
+## Generate the site locally
+
+1. Run a local Jekyll server with make serve
+2. Visualize at `http://localhost:4000/talks/`
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,15 @@
-// Handle foldable challenges and solutions (on click and at start).
-$(".solution>h3,.details>h3").click(function(event) {
-    $(">*:not(h3)", $(this).parent()).toggle(400);
-    $(">span.fold-unfold", this).toggleClass("fa-plus-square fa-minus-square");
-});
+document.addEventListener('DOMContentLoaded', () => {
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
-$(".solution,.details").each(function() {
-    $(">*:not(h3)", this).toggle();
-    var h3 = $("h3:first", this);
-    h3.append("<span class='fold-unfold fa fa-plus-square'></span>");
-});
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach(($el) => {
+      $el.addEventListener('click', () => {
+        const target = $el.dataset.target;
+        const $target = document.getElementById(target);
 
-(function (window, document) {
-    function onDocumentReady(fn) {
-        if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
-            fn();
-        } else {
-            document.addEventListener('DOMContentLoaded', fn);
-        }
-    }
-
-    onDocumentReady(function () {
-        // If you pass `?with-answers` to an URL, it will automatically open
-        // the `<details>` blocks (i.e. the Q&A sections most of the time).
-        if (window.location.search.match(/\?with-answers/gi)) {
-            // Loop over all the `<details>` tags and open them.
-            var details = document.querySelectorAll('details');
-            Array.prototype.forEach.call(details, function (detail) {
-                detail.querySelector('summary').textContent = 'Answers';
-                detail.setAttribute('open', true);
-            });
-        }
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
     });
-
-})(window, document);
+  }
+});
